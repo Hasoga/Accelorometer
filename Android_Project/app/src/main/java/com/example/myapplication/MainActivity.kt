edccac9ac78d6b662ce9dd68087cdc48fe221370
Hasoga <sahanhasoga.15@cse.mrt.ac.lk>
2019-09-15 20:54:33 +0530

    initial

---
+++ Android_Project/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import  android.widget.Button
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.hardware.SensorEvent
+import  android.hardware.SensorEventListener
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.lang.NullPointerException
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+    var output = ""
+    var boolVal = 0
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null) {
+            if (boolVal == 0) {
+                textView.text =
+                    "X = ${event!!.values[0]}\n\n" + "Y = ${event.values[1]}\n\n" + "Z= ${event.values[2]}\n\n"
+            } else {
+                textView.text = ""
+            }
+        }
+        else{
+            textView.text = "Sensor not avail"
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+       start_button.setOnClickListener {
+           boolVal = 0
+           sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+           if (sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null){
+               sensorManager.registerListener(
+                   this,
+                   sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)  ,
+                   SensorManager.SENSOR_DELAY_NORMAL
+               )
+
+               findViewById<TextView>(R.id.textView).apply {
+                   text = output
+               }
+           }
+           else{
+               textView.text = "Sensor Not Available"
+           }
+       }
+
+       stop_button.setOnClickListener {
+           boolVal = 1
+       }
+
+    }
+
+
+}
